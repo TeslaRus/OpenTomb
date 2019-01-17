8781d2d7573f7d566a6a7aba0508204419bb2275
TeslaRus <tesla.rus@gmail.com>
2019-01-17 21:55:51 +0300

    fixed segfault crash;

--- src/world.cpp
+++ src/world.cpp
@@ -760,37 +760,38 @@ struct sprite_s *World_GetSpriteByID(uint32_t ID)
 
 struct skeletal_model_s *World_GetModelByID(uint32_t id)
 {
-    long int i, min, max;
-
-    min = 0;
-    max = global_world.skeletal_models_count - 1;
-    if(global_world.skeletal_models[min].id == id)
+    if(global_world.skeletal_models)
     {
-        return global_world.skeletal_models + min;
-    }
-    if(global_world.skeletal_models[max].id == id)
-    {
-        return global_world.skeletal_models + max;
-    }
-    do
-    {
-        i = (min + max) / 2;
-        if(global_world.skeletal_models[i].id == id)
+        long int i, min, max;
+        min = 0;
+        max = global_world.skeletal_models_count - 1;
+        if(global_world.skeletal_models[min].id == id)
         {
-            return global_world.skeletal_models + i;
+            return global_world.skeletal_models + min;
         }
+        if(global_world.skeletal_models[max].id == id)
+        {
+            return global_world.skeletal_models + max;
+        }
+        do
+        {
+            i = (min + max) / 2;
+            if(global_world.skeletal_models[i].id == id)
+            {
+                return global_world.skeletal_models + i;
+            }
 
-        if(global_world.skeletal_models[i].id < id)
-        {
-            min = i;
+            if(global_world.skeletal_models[i].id < id)
+            {
+                min = i;
+            }
+            else
+            {
+                max = i;
+            }
         }
-        else
-        {
-            max = i;
-        }
+        while(min < max - 1);
     }
-    while(min < max - 1);
-
     return NULL;
 }
 
